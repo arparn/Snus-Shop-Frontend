1e880e6e08ab6139dd4bfa66a5663f218a9c2029
arthur <arthurp200148@gmail.com>
2020-09-28 16:58:37 +0300

    Make item-details component. ref #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule,
