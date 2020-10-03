99b239e0e40fda6eb27809124e543193f595f8f4
Nikita <nibirj@ttu.ee>
2020-10-03 23:31:48 +0300

    #15 worked on header and pictures are still broken

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemsComponent } from './items/items.component';
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { ItemDetailsComponent } from "./item-details/item-details.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:id', component: ItemDetailsComponent },
-  { path: "dashboard", component: DashboardComponent},
-  { path: "items", component: ItemsComponent }
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'items', component: ItemsComponent }
 ];
 
 @NgModule({
