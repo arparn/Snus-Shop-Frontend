4fd3fbd32bcf22586a12098a5520f30f3d4e0d6b
Ilja Boitsuk <ilboit@ttu.ee>
2020-12-28 12:12:30 +0200

    #33 Separate users

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,12 +5,14 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Item } from './items/item';
 import {UserPassword} from './user-password';
 import {User} from "./user";
+import {Info} from "./info";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private userUrl = 'api///user';
+  private userUrl = 'api/user';
   constructor(private http: HttpClient) { }
 
   httpOptions = {
@@ -18,20 +20,32 @@ export class UserService {
   };
 
   addToWishlist(id: number): Observable<Item> {
-        return this.http.post<Item>(`api/wishlist`, id, this.httpOptions);
+  const obj = localStorage.getItem("currentUser");
+    const jsonObj = JSON.parse(obj);
+    const token = jsonObj.token;
+        return this.http.post<Item>(`api/wishlist`, {id, token} as Info, this.httpOptions);
   }
 
   addToShoppingCart(id: number): Observable<Item> {
       const url = `api/cart`;
-      return this.http.post<Item>(url, id, this.httpOptions);
+      const obj = localStorage.getItem("currentUser");
+          const jsonObj = JSON.parse(obj);
+          const token = jsonObj.token;
+      return this.http.post<Item>(url, {id, token} as Info, this.httpOptions);
   }
 
   getShoppingCart(): Observable<ItemCount[]> {
-    return this.http.get<ItemCount[]>(`api/cart`);
+  const obj = localStorage.getItem("currentUser");
+    const jsonObj = JSON.parse(obj);
+    const token = jsonObj.token;
+    return this.http.post<ItemCount[]>(`api/cart/purchase`, token, this.httpOptions);
   }
 
   getWishlist(): Observable<Item[]> {
-    return this.http.get<Item[]>(`api/wishlist`);
+  const obj = localStorage.getItem("currentUser");
+  const jsonObj = JSON.parse(obj);
+  const token = jsonObj.token;
+    return this.http.post<Item[]>(`api/wishlist/list`, token, this.httpOptions);
   }
 
   deleteItemCount(itemCount: ItemCount | number): Observable<ItemCount[]> {
